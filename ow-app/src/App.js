529abd9fc054970bf28fb133b42e15edf94d65d1
Mats Jensen <mats.jensen@evry.com>
2018-02-20 10:03:25 +0100

    layoput of menu is now in place

--- ow-app/src/App.js
+++ ow-app/src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
-import bg from './img/owbg.png';
-import mobilelogo from './img/mobilelogo.png';
 import styled from 'styled-components';
 
-import Navigation from './components/Header';
+//images
+import bg from './img/owbg.png';
+import mobilelogo from './img/mobilelogo.png';
+import logo from './img/logo.png';
+
+// components
+import Navigation from './components/Navigation';
+
+
 
 const baseColor = 'rgba(0, 0, 255, 0.3)';
 
@@ -15,25 +21,65 @@ const Header = styled.header`
 
   @media only screen and (max-width: 700px) {
     height: 220px;
+    font-size: 3.5vw;
   }
 `;
 
 const MobileLogo = styled.img`
   display: grid;
   margin: auto;
+  margin-top: 20px;
 `;
 
+const Headerlogo = styled.img`
+    width: 290px;
+    margin: 0 auto;
+    display: grid;
+    margin-bottom: 20px;
+`;
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      width: window.innerWidth,
+    };
+  }
+  
+  componentWillMount() {
+    window.addEventListener('resize', this.handleWindowSizeChange);
+  }
+  
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowSizeChange);
+  }
+
+  handleWindowSizeChange = () => {
+    this.setState({width: window.innerWidth});
+  }
+
   render() {
-    return (
-      <div className={this.props.className}>
+    const { width } = this.state;
+    const isMobile = window.innerWidth <= 700;
+    if(isMobile) {
+      return (
+        <div className={this.props.className}>
         <Header>
           <Navigation/>
           <MobileLogo src={mobilelogo}  alt="logo"/>
         </Header>
       </div>
-    );
+      );
+    } else {
+      return (
+        <div className={this.props.className}>
+          <Header>
+            <Headerlogo src={logo} alt="Logo"/>
+            <Navigation/>
+          </Header>
+        </div>
+      );
+    }
   }
 }
 
--- ow-app/src/components/Navigation.js
+++ ow-app/src/components/Navigation.js
@@ -1,46 +1,41 @@
+import styled from 'styled-components';
 import React from 'react';
-import styled, { css } from 'styled-components';
-import logo from '../img/logo.png';
 
 const Nav = styled.div`
     color: #fff;
     display: grid;
+    margin: 0px;
 `;
 
 const List = styled.ul`
     display: grid;
     grid-gap: 20px;
-    grid-template-columns: repeat(5, minmax(auto, 1fr));
+    grid-template-columns: repeat(4, 1fr);
     grid-template-rows: 50px;
     align-items: center;
     justify-items: center;
     -webkit-padding-start: 25px;
+    cursor: pointer;
+    margin: auto;
+
+    @media only screen and (max-width: 700px) {
+        margin-left: -8%;
+    }
 `;
 
 const ListItem = styled.li`
     list-style-type: none;
-    font-size: 1.2 vw; 
-`;
-
-const Headerlogo = styled.img`
-    width: 200px;
-
-    @media only screen and (max-width: 700px) {
-        display: none;
-    }
 `;
 
 const Navigation = (props) => {
     return(
         <Nav>
             <List>
-                <Headerlogo src={logo} alt="Logo"/>
                 <ListItem>HEROES</ListItem>
                 <ListItem>OWL</ListItem>
                 <ListItem>PATCHNOTES</ListItem>
                 <ListItem>STATS</ListItem>
             </List>
-            
         </Nav>
     )
 }
